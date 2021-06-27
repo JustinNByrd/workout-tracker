a8d0ee89d536c75ad6540cad3e0036302ef05205
Justin Byrd <justin_byrd@hotmail.com>
2021-06-26 20:03:55 -0400

    Added get /exercise route to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,6 +26,10 @@ app.get("/api/workouts", async (req, res) => {
 		res.json(err);
 	}
 });
+
+app.get("/exercise", (req, res) => (
+	res.sendFile(path.join(__dirname, "public/exercise.html"))
+));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
