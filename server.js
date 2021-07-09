a1e6da4fd3f2bb315434f32c083cf57042b3ea6e
Justin Byrd <justin_byrd@hotmail.com>
2021-07-09 09:06:46 -0400

    add post /api/workouts route to server.js

--- server.js
+++ server.js
@@ -35,6 +35,16 @@ app.get("/exercise", (req, res) => {
 	res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
+app.post("/api/workouts", async (req, res) => {
+	try {
+		const addWorkout = await db.Workout.create( req.body );
+		res.status(200).json(addWorkout);
+	}  catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
 app.put("/api/workouts/:id", async (req, res) => {
 	try {
 		console.dir(req.body);
